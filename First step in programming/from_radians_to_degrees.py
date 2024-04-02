117b01a57801bb23b4cd5838e1f4c6b90e2f0af0
petar-tsekov <165672876+petar-tsekov@users.noreply.github.com>
2024-04-02 06:04:09 -0700

    exercise

---
+++ First step in programming/from_radians_to_degrees.py
@@ -0,0 +1,8 @@
+import math
+
+angel_in_radians = float(input())
+
+degree_total = angel_in_radians * 180 / math.pi
+
+print(degree_total)
+
---
+++ First step in programming/usd_to_bg.py
@@ -0,0 +1,9 @@
+
+income_sum = float(input())
+
+usd_course = 1.79549
+
+total_sum = income_sum * usd_course
+
+print(total_sum)
+
