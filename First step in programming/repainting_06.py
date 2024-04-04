0a26292362b3da7aa3e14802aa660e001c788aea
petar-tsekov <165672876+petar-tsekov@users.noreply.github.com>
2024-04-04 06:08:32 -0700

    Update repainting_06.py
    exercise update

--- First step in programming/repainting_06.py
+++ First step in programming/repainting_06.py
@@ -8,5 +8,14 @@ paint_price = 14.50
 liquid_price = 5
 extra_space = 2
 bags = 0.40
+paint_precent = 10
 
-nylon_total = (nylon_qty + 2)
+
+nylon_total = (nylon_qty + extra_space) * nylon_price
+paint_total = (paint_qty + (paint_qty * paint_precent/100)) * paint_price
+liquid_total = liquid_qty * liquid_price
+total_materials_price = nylon_total + paint_total + liquid_total + bags
+total_worker_price = (total_materials_price * 30/100) * working_hours
+total_price = total_worker_price + total_materials_price
+
+print(total_price)
