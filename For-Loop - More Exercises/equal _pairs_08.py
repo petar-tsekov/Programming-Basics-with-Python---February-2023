2417ee36fe783c49cad5bc515a8d81e69fe16dcc
petar-tsekov <165672876+petar-tsekov@users.noreply.github.com>
2024-05-16 18:12:54 +0300

    Add files via upload

---
+++ For-Loop - More Exercises/equal _pairs_08.py
@@ -0,0 +1,12 @@
+coupels_qty = int(input())
+
+couples_equels = 0
+couples_diff = 0
+
+for couples in range(coupels_qty):
+    couple_a = int(input())
+    couple_b = int(input())
+
+    if couple_a == couple_b:
+        
+
